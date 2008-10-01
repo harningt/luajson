5eddef12f521196d086e16f6d286eba45469d6b3
Thomas Harning Jr <harningt@gmail.com>
2008-10-01 00:11:52 -0400

    encoder: added applicable configuration options from the decoder to the number encoder

--- src/json/encode/number.lua
+++ src/json/encode/number.lua
@@ -1,11 +1,40 @@
 local tostring = tostring
+local assert = assert
+local util = require("json.util")
 
 module("json.encode.number")
 
+local defaultOptions = {
+	nan = true,
+	inf = true,
+-- N/A - cannot tell encoder not to use fractions, exponents, or hex-form
+--	frac = true,
+--	exp = true,
+--	hex = false
+}
+
+default = nil -- Let the buildCapture optimization take place
+strict = {
+	nan = false,
+	inf = false
+}
+
 function encode(number, options)
+	-- Load up number-only options
+	options = options and options.number
+	options = options and util.merge({}, defaultOptions, options) or defaultOptions
 	local str = tostring(number)
-	if str == "nan" then return "NaN" end
-	if str == "inf" then return "Infinity" end
-	if str == "-inf" then return "-Infinity" end
+	if str == "nan" then
+		assert(options.nan, "Invalid number: NaN not enabled")
+		return "NaN"
+	end
+	if str == "inf" then
+		assert(options.inf, "Invalid number: Infinity not enabled")
+		return "Infinity"
+	end
+	if str == "-inf" then
+		assert(options.inf, "Invalid number: -Infinity not enabled")
+		return "-Infinity"
+	end
 	return str
 end
