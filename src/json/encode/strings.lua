d91e0d76593ab7e469681b58b935e82dbf26da14
Thomas Harning Jr <harningt@gmail.com>
2009-01-12 21:09:05 -0500

    encoder: adds \xXX encoding and customizable encoding set

--- src/json/encode/strings.lua
+++ src/json/encode/strings.lua
@@ -1,9 +1,10 @@
 local string_char = string.char
+local pairs = pairs
 
 local util_merge = require("json.decode.util").merge
 module("json.encode.strings")
 
-local encodingMap = {
+local normalEncodingMap = {
 	['\\'] = '\\\\',
 	['"'] = '\\"',
 	['\n'] = '\\n',
@@ -15,20 +16,29 @@ local encodingMap = {
 	['/'] = '\\/'
 }
 
+local xEncodingMap = {}
+for char, encoded in pairs(normalEncodingMap) do
+	xEncodingMap[char] = encoded
+end
+
 -- Pre-encode the control characters to speed up encoding...
 -- NOTE: UTF-8 may not work out right w/ JavaScript
 -- JavaScript uses 2 bytes after a \u... yet UTF-8 is a
 -- byte-stream encoding, not pairs of bytes (it does encode
 -- some letters > 1 byte, but base case is 1)
-for i = 1, 255 do
+for i = 0, 255 do
 	local c = string_char(i)
-	if c:match('%c') and not encodingMap[c] then
-		encodingMap[c] = ('\\u%.4X'):format(i)
+	if c:match('[%c\128-\255]') and not normalEncodingMap[c] then
+		normalEncodingMap[c] = ('\\u%.4X'):format(i)
+		xEncodingMap[c] = ('\\x%.2X'):format(i)
 	end
 end
 
 local defaultOptions = {
-	preProcess = false
+	preProcess = false,
+	xEncode = false, -- Encode single-bytes as \xXX
+	encodeSet = '\\"/%c%z',
+	encodeSetAppend = nil -- Chars to append to the default set
 }
 
 default = nil
@@ -37,11 +47,16 @@ strict = nil
 function getEncoder(options)
 	options = options and util_merge({}, defaultOptions, options) or defaultOptions
 	local stringPreprocess = options and options.preProcess
+	local encodeSet = options.encodeSet
+	if options.encodeSetAppend then
+		encodeSet = encodeSet .. options.encodeSetAppend
+	end
+	local encodingMap = options.xEncode and xEncodingMap or normalEncodingMap
 	local function encodeString(s, state)
 		if stringPreprocess then
 			s = stringPreprocess(s)
 		end
-		return '"' .. s:gsub('[\\"/%c%z]', encodingMap) .. '"'
+		return '"' .. s:gsub('[' .. encodeSet .. ']', encodingMap) .. '"'
 	end
 	return {
 		string = encodeString
