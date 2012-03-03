4c499cd447a1b09ac258cbb93b7572b92b460ef4
Thomas Harning Jr <harningt@gmail.com>
2012-03-03 15:09:26 -0500

    all: adds module metadata as suggested by fperrad

--- lua/json.lua
+++ lua/json.lua
@@ -7,6 +7,9 @@ local encode = require("json.encode")
 local util = require("json.util")
 
 module("json")
+_VERSION = "1.0.3"
+_DESCRIPTION = "LuaJSON : customizable JSON decoder/encoder"
+_COPYRIGHT = "Copyright (c) 2007-2012 Thomas Harning Jr. <harningt@gmail.com>"
 _M.decode = decode
 _M.encode = encode
 _M.util = util
